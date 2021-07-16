135d2aa9be9b4bfcad80ac30a9a907a521654f45
kskobayashi <35519174+kskobayashi@users.noreply.github.com>
2021-07-17 00:41:34 +0900

    Add files via upload

---
+++ rsswidget.php
@@ -0,0 +1,49 @@
+<style>
+    div {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        max-width: 100%;
+        margin-top: 10px;
+        font-family: sans-serif;
+        font-size: 14px;
+    }
+
+    .date {
+        color: darkgray;
+        margin: 0 10px 0 10px;
+    }
+
+    a:link {
+        color: rgba(45, 45, 93, 1.0);
+    }
+</style>
+
+<?php
+$parame = $_GET['url'];
+$rss = simplexml_load_file($parame);
+
+$end = 3; //Display n lines.
+
+$i = 0;
+foreach ($rss->channel->item as $item) {
+
+    $title = $item->title;
+    $date = date("m.d", strtotime($item->pubDate));
+    $link = $item->link;
+    $description = strip_tags($item->description);
+    $i++;
+?>
+    <div>
+        <span class="date"><?php echo $date; ?></span>
+        <a href="<?php echo $link; ?>" target="_blank">
+            <span class="title"><?php echo $title; ?></span>
+            <span class="text"><?php echo $description; ?></span>
+        </a>
+    </div>
+<?php
+    if ($i === $end) {
+        break;
+    }
+}
+?>
